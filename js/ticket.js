212fc3e23ed4fb60be53152d98540d82b46302be
songganam <dev.ganam@gmail.com>
2023-11-09 12:18:41 +0900

    feat : 퍼블리싱 진행
    - ticket.js
    - index.html > css
    - event.css

--- js/ticket.js
+++ js/ticket.js
@@ -22,12 +22,39 @@ window.addEventListener("load", function () {
     for (let i = 0; i < _res.total; i++) {
       const index = i + 1;
       const obj = _res["good_" + index];
+      const tempTag = `
+      <div class="swiper-slide">
+      <div class="ticket-slide-item">
+        <a href="${obj.url}" class="ticket-link">
+          <div class="ticket-img">
+            <img src="${obj.image}" alt="${obj.desc}" />
+          </div>
+        </a>
+      </div>
+      <div class="ticket-info">
+        <ul class="ticket-good-list">
+          <li>
+            <span class="ticket-good-info-price">
+              <em>${obj.title}</em>
+              <p>${obj.place}</p>
+              <i>${obj.date}</i>
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>
+      `;
+      htmlTicketTag += tempTag;
     }
+    showHtmlTag(htmlTicketTag);
+  }
+  
+  function showHtmlTag(_html) {
+    const ticketSlide = ".ticket-slide .swiper-wrapper";
+    const tag = document.querySelector(ticketSlide);
+    tag.innerHTML =
   }
   // 2. html 태그를 백틱을 이용해서 만든다.
-  const tempTag = `
-
-  `;
 
   // 3. swiper 태그에 백틱을 배치한다.
   const reommendSlide = ".ticket-slide .slide-wrapper";
@@ -35,3 +62,19 @@ window.addEventListener("load", function () {
   // 4. swiper 작동시킨다.
   //   const ticket Swiper = new Swiper(".ticket-slide");
 });
+
+function makeSwiper() {
+ const swiperTicket = new Swiper (".Ticket-slide", {
+  slidePerView: 4,
+  spaceBetween: 27,
+  navigation: {
+    nextEl: ".ticket-slide-wrap .slide-next-bt",
+    prevEl: ".ticket-slide-wrap .slide-prev-bt",
+  },
+  slidesPerGroup: 4,
+
+ }
+
+ )
+
+}
